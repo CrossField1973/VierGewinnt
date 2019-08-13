3a17cf6d999dd8aec04527c52b3fb7c092d43ae3
Dorian Riepe <44470917+dorianriepe@users.noreply.github.com>
2019-08-13 14:54:34 +0200

    Update user_table.sql

--- SQL-Scripts/user_table.sql
+++ SQL-Scripts/user_table.sql
@@ -1,32 +1,14 @@
--- phpMyAdmin SQL Dump
+-- phpMyAdmin
 -- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 02, 2019 at 11:14 AM
+
 -- Server version: 10.1.40-MariaDB
--- PHP Version: 7.3.5
+-- PHP Version: 7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `user_table`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_table`
---
 
 CREATE TABLE `user_table` (
   `name` varchar(20) COLLATE latin1_german1_ci DEFAULT NULL,
@@ -39,27 +21,10 @@ CREATE TABLE `user_table` (
   `ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `user_table`
---
 ALTER TABLE `user_table`
   ADD PRIMARY KEY (`ID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `user_table`
---
 ALTER TABLE `user_table`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
