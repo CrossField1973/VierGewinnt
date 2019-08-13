5498f3dfe890a5c0b0ce607d7df16725bd560159
MMIT-kewe <45197889+MMIT-kewe@users.noreply.github.com>
2019-08-13 23:12:34 +0200

    added update_page function

--- JS/game.js
+++ JS/game.js
@@ -4,3 +4,8 @@ function register_turn(index)
     window.location.href = "http://192.168.92.103/VierGewinnt-Login/HTML/game.php?turn=" + index;
 }
 
+function update_page()
+{
+    window.location.href = "http://192.168.92.103/VierGewinnt-Login/HTML/game.php";
+}
+
