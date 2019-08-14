6173816bae0483be72abdd1453acc082ce1e0555
Dorian Riepe <44470917+dorianriepe@users.noreply.github.com>
2019-08-14 23:35:14 +0200

    Show account information

--- PHP/settings.php
+++ PHP/settings.php
@@ -1,7 +1,37 @@
+<?php
+        
+        $token = $_COOKIE["token"];
+
+        $sqlhost = "localhost";
+        $sqluser = "root";
+        $sqlpass = "";
+        $dbname  = "user_table";
+
+        $my_db = mysqli_connect($sqlhost, $sqluser, $sqlpass, $dbname) or die ("DB-system nicht verfuegbar");
+
+        if (mysqli_connect_error()) {
+          die('Connection Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+        } else {
+
+          $READ = "SELECT * FROM user_table WHERE securitytoken='".$token."'";
+          $result = mysqli_query($my_db, $READ);
+          
+          $row = $result->fetch_assoc();
+          
+        
+        }
+
+?>
+
+
+
+
+
+
 <html>
 
 <head>
-  <title>Viergewinnt Settings</title>
+  <title>Settings</title>
   <link rel="stylesheet" href="../CSS/register.css">
 </head>
 
@@ -9,25 +39,34 @@
   <form action="../PHP/settings.php" method="post">
     <div class="register-box">
       <h1>Settings</h1>
-      <div class="textbox">
-        <input type="email" placeholder="Email" name="email" value="" required readonly>
-      </div>
+      
+      <?php echo "<div class='textbox'>
+        <input type='email' placeholder='Email' name='email' value='".$row['email']."' required readonly>
+        </div>"
+      ?>
+    
       <div class="textbox">
         <input type="password" placeholder="Password *" name="pw" value="" required>
       </div>
-      <div class="textbox">
-        <input type="text" placeholder="Nickname" name="nickname" value="" required>
-      </div>
+      
+      <?php echo  "<div class='textbox'>
+        <input type='text' placeholder='Nickname' name='nickname' value=".$row['nickname']." required>
+      </div>"
+      ?>
 
-      <div class="textbox">
-        <input type="text" placeholder="Firstname" name="fname" value="" readonly>
+      <?php echo  "<div class='textbox'>
+        <input type='text' placeholder='Firstname' name='fname' value='".$row['first_name']."' readonly>
       </div>
-      <div class="textbox">
-        <input type="text" placeholder="Name" name="name" value="" readonly>
-      </div>
-      <div class="textbox">
-        <input type="number" placeholder="Age" name="age" value="" min="18" max="100">
-      </div>
+      <div class='textbox'>
+        <input type='text' placeholder='Name' name='name' value=".$row['name']." readonly>
+      </div>"
+      ?>
+      
+      <?php echo  "<div class='textbox'>
+        <input type='number' placeholder='Age' name='age' value=".$row['age']." min='18' max='100'>
+      </div>"
+      ?>
+
       <div>
         <label class="container">No information
           <input type="radio" checked="checked" name="radio" value="def">
