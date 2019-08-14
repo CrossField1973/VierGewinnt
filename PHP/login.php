371923cf4aa368e8baa7f61386496043789da9f9
Dorian Riepe <44470917+dorianriepe@users.noreply.github.com>
2019-08-14 22:56:40 +0200

    Added cookies for user login

--- PHP/login.php
+++ PHP/login.php
@@ -7,19 +7,39 @@ $dbname  = "user_table";
 $my_db = mysqli_connect($sqlhost, $sqluser, $sqlpass, $dbname) or die("DB-system nicht verfuegbar");
 
 if ((isset($_GET['email'])) && (isset($_GET['pw']))) {
+  
   $safe_email = mysqli_real_escape_string($my_db, $_GET['email']);
   $sql = "SELECT * FROM user_table WHERE email = '" . $safe_email . "';";
   $res = mysqli_query($my_db, $sql) or die(mysqli_error($my_db));
+  
   if ($dbdata = mysqli_fetch_assoc($res)) {
+  
     if (password_verify($_GET['pw'], $dbdata['password'])) {
+      
+      
+          $READ = "SELECT securitytoken FROM user_table WHERE email='".$safe_email."'";
+          $result = mysqli_query($my_db, $READ);
+          
+          $row = $result->fetch_assoc();
+          $token = $row['securitytoken'];
+        
+          setcookie("token", $token, time() + (86400 * 30), "/");
+
+        }
+
+
+
       $message = "Signed In";
       echo "<script type='text/javascript'>alert('$message');</script>";
-      header("Location: ../HTML/lobby.html");
+      header("Location: ../PHP/lobby.php");
+    
     } else{
+      
       $message = "Wrong Email or Password";
       echo "<script type='text/javascript'>alert('$message');</script>";
       header("Location: ../HTML/login.html");
+    
     }
   }
-}
+
 ?>
