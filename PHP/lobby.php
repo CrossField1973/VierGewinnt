e8db6419e6eef8a632e2f3edbdd5198661117873
MMIT-kewe <45197889+MMIT-kewe@users.noreply.github.com>
2019-08-16 18:36:30 +0200

    made it functional
    changed navigation bar, added functionality, redirect if user isn't logged in

--- PHP/lobby.php
+++ PHP/lobby.php
@@ -1,84 +1,124 @@
+<?php
+	if(isset($_COOKIE["token"]) && $_COOKIE["token"] != "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC;")
+	{
+		$token = $_COOKIE["token"];
+	}
+
+	else
+	{
+		header("Location: http://example.com/error.php");
+        die();
+	}
+
+	$sqlhost = "localhost";
+	$sqluser = "root";
+	$sqlpass = "";
+	$dbname  = "viergewinnt";
+
+	$my_db = mysqli_connect($sqlhost, $sqluser, $sqlpass, $dbname) or die ("DB-system nicht verfuegbar");
+
+	if (mysqli_connect_error()) {
+		die('Connection Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+	}
+?>
+
 <html>
-    <head>
-        <title>Lobby</title>
-        <link rel="stylesheet" href="../CSS/lobby.css">
-        </head>
-        <body>
-		<script>
-		function addAChild () {
-		  var myBox = document.createElement('div');
-		  myBox.setAttribute("class","game1");
-		  var myBox2 = document.createElement('div');
-		  var myIdText = document.createTextNode('Game 1')
-		  myBox2.setAttribute("class","gameid");
-		  myBox2.appendChild(myIdText);
-		  myBox.appendChild(myBox2);
-		  var myBtn = document.createElement('button');
-		  myBtn.setAttribute("type","button");
-		  myBtn.setAttribute("class","joinbtn");
-		  var myBtnText = document.createTextNode('JOIN');
-		  myBtn.appendChild(myBtnText);
-		  var myCre = document.createElement('div');
-		  myCre.setAttribute("class","creator");
-		  var myText = document.createTextNode('Erstellt von *User*');
-		  myCre.appendChild(myText);
-		  
-		  var Ausgabebereich = document.getElementsByClassName('lobby-box');
-		  var add = document.getElementById('add');
-			Ausgabebereich[0].insertBefore(myBox, add);
-			myBox.appendChild(myBtn);
-			myBox.appendChild(myCre);
-		}
+	<head>
+		<title>Lobby</title>
+		<link rel="stylesheet" href="../CSS/lobby.css">
+		<script src="../JS/lobby.js"></script>
+	</head>
 		
-		function logout() {
-			document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-			alert("Logout successfully!");
-		}
-		</script>
-			<div class="dashboard">
-				<div class="logo">4GEWINNT</div>
-				<a href="../HTML/login.html"><button type="button" class="logoutbtn" onclick="logout()">Logout</button></a>
-				<a href="../PHP/settings.php"><button type="button" class="sbtn">Settings</button></a>
-				<!--<a href="lobby.php"><button type="button" class="homebtn">Home</button></a>-->
+    <body>
+		<!--Navigation Bar-->
+		<ul class="dashboard">
+			<li class="logo" style="display: inline">
+				<a href="http://192.168.92.106/VierGewinnt-Login/PHP/lobby.php">
+					<img src="../IMG/4gewinnt_logo.png" style="height: 100%; width: auto">
+				</a>
+			</li>
+			<li style="display: inline">
+				<a href="../HTML/login.html">
+					<button type="button" class="logoutbtn" onclick="logout()">
+						Logout
+					</button>
+				</a>
+			</li>
+			<li style="display: inline">
+				<a href="../PHP/settings.php">
+					<button type="button" class="sbtn">
+						Settings
+					</button>
+				</a>
+			</li>
+			<li style="display: inline">
+				<a href="game.php">
+					<button type="button" class="logoutbtn">
+						Continue Game
+					</button>
+				</a>
+			</li>
+		</ul>
+	
+		<!--Main Area-->
+		<div class="lobby-box">
+			<h1 class="titel">Lobby</h1>
+			<div class="welbox">
+				Welcome back 
+
+				<?php
+					$READ = "SELECT nickname FROM user_table WHERE securitytoken='".$token."'";
+					$result = mysqli_query($my_db, $READ);
+					$row = $result->fetch_assoc();
+					echo $row['nickname'];
+				?>
 			</div>
-            <div class="lobby-box">
-			
-                <h1 class="titel">Lobby</h1>
-				<div class="welbox">
-					Welcome back 
 
-					<?php 
+			<div>
+				<!--List of joinable Games-->
+				<?php
+					$sql = "SELECT red FROM games";
+					$result = $my_db->query($sql);
+				
+					if ($result->num_rows > 0) 
+					{
+						for($i = 1; $i <= $result->num_rows; $i++)
+						{
+							$row = $result->fetch_assoc();
+						echo "<div class='game1'><div class='gameid'>Game ".$i."</div><button type='button' class='joinbtn'>JOIN</button><div class='creator'>Erstellt von ".$row["red"]."</div></div>";
+						}
+					}
+				?>				
 
-
-        			$token = $_COOKIE["token"];
-
-        			$sqlhost = "localhost";
-        			$sqluser = "root";
-        			$sqlpass = "";
-        			$dbname  = "user_table";
-
-        			$my_db = mysqli_connect($sqlhost, $sqluser, $sqlpass, $dbname) or die ("DB-system nicht verfuegbar");
-
-        			if (mysqli_connect_error()) {
-          				die('Connection Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-        			} else {
-
-          			$READ = "SELECT first_name FROM user_table WHERE securitytoken='".$token."'";
-          			$result = mysqli_query($my_db, $READ);
-          
-          			$row = $result->fetch_assoc();
-          			echo $row['first_name'];
-        
-        			}
-
-        			?>
-
-
+				<!--Open new Game Button-->
+				<div class="gameadd" id="add">	
+					<button type="button" class="addbtn"><a href="lobby.php?add=true">+</a></button>			
+					<!--<button type="button" class="addbtn" onclick="addAChild()">+</button>-->
 				</div>
-        
-				<div class="gameadd" id="add">				
-					<button type="button" class="addbtn" onclick="addAChild()">+</button>
-				</div>
-            </div>
-        </body>
+			</div>
+		</div>
+
+		<!--Functionality of the Add Button-->
+		<?php
+			if(isset($_GET["add"]))
+			{
+				//Check if user has an open game
+				$sql = "SELECT red FROM games WHERE red = '".$_SERVER['REMOTE_ADDR']."' OR yellow = '".$_SERVER['REMOTE_ADDR']."'";
+				$result = $my_db->query($sql);
+
+				if ($result->num_rows == 1) 
+				{
+					echo "You are already part of a game_session";
+				} 
+
+				else if($result->num_rows == 0)
+				{
+					echo "here";
+					$sql = "INSERT INTO games (red, turn, state_of_game) VALUES ('".$_SERVER['REMOTE_ADDR']."', '".$_SERVER['REMOTE_ADDR']."', '000000000000000000000000000000000000000000')";
+					$my_db->query($sql);
+					header('Location: '.$_SERVER['REQUEST_URI']);
+				}
+			}
+		?>
+	</body>
 </html>
