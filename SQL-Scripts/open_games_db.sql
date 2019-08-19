fa0bac5d26aa556ef76f8d3964f3add6d6304d96
MMIT-kewe <45197889+MMIT-kewe@users.noreply.github.com>
2019-08-19 14:45:04 +0200

    Changed IP to securitytoken

--- SQL-Scripts/open_games_db.sql
+++ SQL-Scripts/open_games_db.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `viergewinnt`.`games` ( `id` INT NOT NULL AUTO_INCREMENT , `state_of_game` VARCHAR(42) NOT NULL , `red` VARCHAR(15) NOT NULL , `yellow` VARCHAR(15) NOT NULL , `turn` VARCHAR(15) NOT NULL , UNIQUE (`id`)) ENGINE = InnoDB;
+CREATE TABLE `viergewinnt`.`games` ( `id` INT NOT NULL AUTO_INCREMENT , `state_of_game` VARCHAR(42) NOT NULL , `player1` VARCHAR(255) NOT NULL , `player2` VARCHAR(255) NOT NULL , `turn` VARCHAR(255) NOT NULL , UNIQUE (`id`)) ENGINE = InnoDB;
