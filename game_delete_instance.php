88b538942622574404f2c24cb2efdb14cb257ade
MMIT-kewe <45197889+MMIT-kewe@users.noreply.github.com>
2019-08-22 01:05:24 +0200

    Delete game_delete_instance.php

--- game_delete_instance.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require "db_connection.php";
-    $token = $_COOKIE["token"];
-
-    $sql = "DELETE FROM games WHERE player1 = '".$token."' OR player2 = '".$token."'";
-    $conn->query($sql);
-?>
