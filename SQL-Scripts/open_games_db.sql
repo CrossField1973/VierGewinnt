a6fdfd2e9ba4da91e37ad9202dc1060596f218f5
MMIT-kewe <45197889+MMIT-kewe@users.noreply.github.com>
2019-08-22 13:32:04 +0200

    Update open_games_db.sql

--- SQL-Scripts/open_games_db.sql
+++ SQL-Scripts/open_games_db.sql
@@ -1,1 +1,9 @@
-CREATE TABLE `viergewinnt`.`games` ( `id` INT NOT NULL AUTO_INCREMENT , `state_of_game` VARCHAR(42) NOT NULL , `player1` VARCHAR(255) NOT NULL , `player2` VARCHAR(255) NOT NULL , `turn` VARCHAR(255) NOT NULL , UNIQUE (`id`)) ENGINE = InnoDB;
+CREATE TABLE `games` ( 
+  `id` INT NOT NULL AUTO_INCREMENT , 
+  `state_of_game` VARCHAR(42) NOT NULL , 
+  `player1` VARCHAR(255) NOT NULL , 
+  `player2` VARCHAR(255) NOT NULL , 
+  `turn` VARCHAR(255) NOT NULL , 
+  PRIMARY KEY (`id`)) 
+   
+ENGINE = InnoDB;
