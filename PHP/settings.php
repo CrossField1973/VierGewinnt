b6cac4a4c6262bee4b924b0c21a8dd5f9a22b3a1
MMIT-kewe <45197889+MMIT-kewe@users.noreply.github.com>
2019-08-23 15:09:44 +0200

    added image functionality

--- PHP/settings.php
+++ PHP/settings.php
@@ -14,6 +14,7 @@
 <head>
   <title>Settings</title>
   <link rel="stylesheet" href="../CSS/settings.css">
+  <script src="../JS/settings.js"></script>
 </head>
 
 <body>
@@ -27,8 +28,8 @@
     <li style="display: inline">
       <div class="dropdown" style="height: 100%; width: auto; float: right">
         <?php
-		echo "<img src='".$row["picture_filepath"]."' class='dropbtn' style='max-height: 100%; width: auto;'>";
-	?>
+		      echo "<img src='".$row["picture_filepath"]."' class='dropbtn' style='max-height: 100%; width: auto;'>";
+	      ?>
         <div class="dropdown-content" style="right: 0">
           <a href="login.php" onclick="logout()">Logout</a>
         </div>
@@ -36,10 +37,17 @@
     </li>
   </ul>
 
-  <form action="../PHP/updatesettings.php" method="post">
+  <form action="../PHP/updatesettings.php" method="post" enctype="multipart/form-data">
     <div class="register-box">
       <h1>Settings</h1>
-      
+      <p id="error"></p>
+      <?php echo "<div class='image_upload' style='position: relative; text-align: center;'>
+      <img src='".$row["picture_filepath"]."' id='image_preview' style='max-height: 7vw; width: auto; display: block; margin-left: auto; margin-right: auto;'>
+      <label for='image' style='position: absolute; bottom: 8px; left: 50%; color: black'>Change image</label>
+      <input type='file' name='image' id='image' hidden onchange='javascript:refresh_image()' accept=image/*'>
+      </div>";
+      ?>
+
       <?php echo "<div class='textbox'>
         <input type='email' placeholder='Email' name='email' value='".$row['email']."' required readonly>
         </div>"
